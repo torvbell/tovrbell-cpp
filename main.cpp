f195366a6468f48420983770e09dea5b125c1682
2B键盘 <2968640488@qq.com>
2021-01-25 00:03:52 +0800

    feat: 新增注释

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 
+/**
+ * 比较大小
+ * @param n_1 第一个数字
+ * @param n_2 第二个数字
+ * @return 最大的那个数字
+ */
 int max(int n_1, int n_2)
 {
     if(n_1 > n_2)
@@ -8,6 +14,7 @@ int max(int n_1, int n_2)
 }
 
 int main() {
-    std::cout << max(10, 20) << std::endl;
+    int max_number = max(10, 20);
+    std::cout << "最大的数值为[" << max_number << "]" << std::endl;
     return 0;
 }
