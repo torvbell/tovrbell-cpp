b0abaf11b2fcdee62ca9d3ad0b68dd2c7e63d78d
2B键盘 <2968640488@qq.com>
2021-01-27 21:43:02 +0800

    feat(io): 新增io写入文件打开判断

--- io/torvbell-wio.cpp
+++ io/torvbell-wio.cpp
@@ -4,3 +4,16 @@
 // Created by 吴彦祖 on 2021/1/27.
 //
 
+#include <iostream>
+#include <ostream>
+#include <fstream>
+
+void write(char * path, char * value)
+{
+    std::fstream file(path);
+
+    if(!file.is_open())
+    {
+        std::cerr << "文件打开失败" << std::endl;
+    }
+}
